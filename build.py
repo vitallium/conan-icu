8160318d5b0ddd834019c80203de9b24f047e78f
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-11-02 12:38:45 +0300

    Use Conan packager

--- build.py
+++ build.py
@@ -1,17 +1,7 @@
-import os, sys
-import platform
-
-def system(command):
-   retcode = os.system(command)
-   if retcode != 0:
-       raise Exception("Error while executing:\n\t %s" % command)
+from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-   system('conan export vitallium/stable')
-   params = " ".join(sys.argv[1:])
-
-   if platform.system() == "Windows":
-       for arch in ["x86", "x86_x64"]:
-           system('conan test -s compiler="Visual Studio" -s arch=%s -s compiler.version=14 %s' % (arch, params))
-   else:
-       pass
+    visual_versions = [14]
+    builder = ConanMultiPackager(username="vitallium")
+    builder.add_common_builds(visual_versions=visual_versions)
+    builder.run()

be2b2d6a23586f32aa507fbfb1020842d2bc39dd
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-11-02 12:39:18 +0300

    Copy DLLs to bin directory for the test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 import os
 
 channel = os.getenv("CONAN_CHANNEL", "testing")
-username = os.getenv("CONAN_USERNAME", "demo")
+username = os.getenv("CONAN_USERNAME", "vitallium")
 
 class IcuConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -13,6 +13,9 @@ class IcuConan(ConanFile):
         cmake = CMake(self.settings)
         self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
+
+    def imports(self):
+        self.copy(pattern="*.dll", dst="bin", src="bin")
 
     def test(self):
         self.run(os.sep.join([".","bin", "tst_icu"]))
